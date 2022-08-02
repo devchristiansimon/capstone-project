f24669052011c06cae54306ac2a4fe55c0bdcdb6
devchristiansimon <inonlinoid@gmail.com>
2022-08-02 20:13:50 +0200

    added title and responsive video player in modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,50 @@
 import styled from 'styled-components';
-import {ContentCard} from './styles/ContentCard';
 
 export function Modal({isOpen, onClose, content}) {
+  const playerStyle = {
+    padding: '56.25% 0 0 0',
+    position: 'relative',
+  };
+
+  const iframeStyler = {
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  };
+
   return (
     isOpen && (
       <StyledModal>
-        {content.id};<button onClick={onClose}>Close</button>
+        <h2>{content.videoName}</h2>
+        <div style={playerStyle}>
+          <iframe
+            src={content.videoUrl}
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            style={iframeStyler}
+            title={content.videoName}
+          ></iframe>
+        </div>
+        <button onClick={onClose}>Close</button>
       </StyledModal>
     )
   );
 }
 
 const StyledModal = styled.div`
+  display: flex;
+  text-align: center;
+  flex-flow: column nowrap;
+  justify-content: space-between;
   position: fixed;
   z-index: 1;
-  top: 20px;
-  width: 100vw;
-  height: 100vh;
-  background-color: black;
-  color: orange;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  height: 90vh;
+  background-color: rgba(24, 24, 24, 0.95);
+  color: white;
 `;
